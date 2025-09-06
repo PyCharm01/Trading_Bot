e288a17055bc9b34f2fb80f6390b3bd48e76a7c2
Shekhar Chormale <93048096+PyCharm01@users.noreply.github.com>
2025-09-06 19:46:31 +0530

    Delete src/config directory

--- src/config/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Configuration management for the Indian market trading platform.
-"""
-
-from .config import get_config, get_indian_market_strategies, get_lot_size, get_margin_requirements
-
-__all__ = ['get_config', 'get_indian_market_strategies', 'get_lot_size', 'get_margin_requirements']
